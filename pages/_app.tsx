851868260dfec4dfcae1034e0644f42a6988c61a
Alex MacCaw <alex@alexmaccaw.com>
2022-05-08 11:32:37 -0400

    title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,7 +22,7 @@ function MyApp({Component, pageProps}: AppProps) {
           name="twitter:image"
           content="https://thebeginningofinfinity.xyz/twitter-card.png"
         />
-        <meta name="og:title" content="Reflect" />
+        <meta name="og:title" content="The Beginning of Infinity" />
         <meta
           name="og:image"
           content="https://thebeginningofinfinity.xyz/twitter-card.png"
