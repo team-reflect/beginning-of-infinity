7923839fc746c441eec933a5986eb6eac426ea9f
Alex MacCaw <alex@alexmaccaw.com>
2022-05-08 12:49:08 -0400

    fix link

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,13 +1,18 @@
+import Link from 'next/link'
 import React from 'react'
 
 export const Header: React.FC = () => (
   <div className="flex-none h-[60px] space-x-5 flex items-center justify-between border-b select-none md:px-8 px-5">
     <h1 className="md:text-xl text-sm font-medium">
-      <a href="/">The Beginning of Infinity</a>
+      <Link href="/">
+        <a>The Beginning of Infinity</a>
+      </Link>
     </h1>
 
     <h3 className="md:text-base text-sm font-normal">
-      <a href="/About">About these notes</a>
+      <Link href="/about">
+        <a>About these notes</a>
+      </Link>
     </h3>
   </div>
 )
